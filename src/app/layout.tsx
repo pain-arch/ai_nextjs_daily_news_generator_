e9f79035b43e4d00c60f6159fd0e787e7d05570f
pain-arch <himulol123@gmail.com>
2024-10-12 20:12:18 +0600

    Refactor import paths and add Footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,7 +32,7 @@ export default function RootLayout({
       >
        <Navbar/>
         {children}
-        <footer></footer>
+        <Footer />
       </body>
     </html>
   );
