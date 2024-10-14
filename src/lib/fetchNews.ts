f22bb1e0a9b36cddbcecc43af5c4a91d8ba928e8
pain-arch <himulol123@gmail.com>
2024-10-14 12:31:28 +0600

    Add fetchNews function for fetching news based on category and search parameters

---
+++ src/lib/fetchNews.ts
@@ -0,0 +1,12 @@
+export const fetchNews = async (category: string = "", search: string = "") => { 
+    try {
+        const response = await fetch(`https://news-api-next-js-kappa.vercel.app/api/news?category=${category}&search=${search}`);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch news: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+     console.error("error fetching data",error);   
+    }
+}
